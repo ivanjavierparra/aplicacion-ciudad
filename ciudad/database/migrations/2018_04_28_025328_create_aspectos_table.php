b06cdacdbcc18b8cd1a870ef657d8ee14acdae82
Matías Acosta <matiasacosta1995@gmail.com>
2018-04-28 16:33:35 -0300

    Inicializacion del proyecto

---
+++ ciudad/database/migrations/2018_04_28_025328_create_aspectos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAspectosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aspectos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('fecha');
+            $table->mediumText('descripcion');
+            $table->float('latitud', 11, 3);
+            $table->float('longitud', 11, 3);
+            $table->unsignedInteger('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aspectos');
+    }
+}
